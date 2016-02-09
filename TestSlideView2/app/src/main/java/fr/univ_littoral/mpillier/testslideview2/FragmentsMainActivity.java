c89514329dcb6ea0fe60e091f8732c29c386f5d8
Skymax84 <skyoxide84@hotmail.fr>
2016-02-09 21:20:50 +0100

    ajout FragmentsMain

--- TestSlideView2/app/src/main/java/fr/univ_littoral/mpillier/testslideview2/FragmentsMainActivity.java
+++ TestSlideView2/app/src/main/java/fr/univ_littoral/mpillier/testslideview2/FragmentsMainActivity.java
@@ -1,19 +1,39 @@
 package fr.univ_littoral.mpillier.testslideview2;
 
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.io.PrintWriter;
+import java.net.InetAddress;
+import java.net.Socket;
+import java.net.UnknownHostException;
 import java.util.List;
 import java.util.Vector;
 
 import android.os.Bundle;
+import android.os.Handler;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentActivity;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.PagerAdapter;
 import android.support.v4.view.ViewPager;
+import android.view.View;
+import android.widget.ImageView;
 
 public class FragmentsMainActivity extends FragmentActivity {
 
     private PagerAdapter mPagerAdapter;
+    private Socket socket;
+    private static final int SERVERPORT = 6666;
+    private static final String SERVER_IP = "10.0.2.2";
+    InputStream stream = null;
+
+    ImageView iv;
+    Handler handlerTest;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -37,5 +57,87 @@ public class FragmentsMainActivity extends FragmentActivity {
 
         // Affectation de l'adapter au ViewPager
         pager.setAdapter(this.mPagerAdapter);
+
+        iv = (ImageView) findViewById(R.id.imagePioche);
+
+        new Thread(new ClientThread()).start();
+    }
+
+    /******************************************
+     * DEBUT THREAD
+     **************************************/
+    class ClientThread implements Runnable {
+
+        @Override
+        public void run() {
+            try {
+                InetAddress serverAddr = InetAddress.getByName(SERVER_IP);
+
+                socket = new Socket(serverAddr, SERVERPORT);
+
+                System.out.println("socket client créée ! ");
+
+                // le client donne son pseudo au serveur
+                PrintWriter out = new PrintWriter(new BufferedWriter(
+                        new OutputStreamWriter(socket.getOutputStream())),
+                        true);
+                out.println("clientTest");
+
+                // mise sur écoute du client
+                stream = socket.getInputStream();
+                BufferedReader reader = new BufferedReader(new InputStreamReader(
+                        stream));
+                String line = "";
+                int i = 0;
+                while ((line = reader.readLine()) != null) {
+                    System.out.println("Serveur dit : " + line);
+                    switch (line) {
+                        case "actionClicPiocheDonjon":
+                            runOnUiThread(new Runnable() {
+                                @Override
+                                public void run() {
+                                    test2();
+                                }
+                            });
+
+                            break;
+                    }
+                }
+
+            } catch (UnknownHostException e1) {
+                e1.printStackTrace();
+            } catch (IOException e1) {
+                e1.printStackTrace();
+            }
+        }
+
+        public void test2() {
+
+            iv = (ImageView) findViewById(R.id.imagePioche);
+
+            iv.setImageResource(R.drawable.image_test);
+
+        }
+    }
+
+    /******************************************
+     * FIN THREAD
+     **************************************/
+
+    public void test(View v) {
+
+        try {
+            PrintWriter out = new PrintWriter(new BufferedWriter(
+                    new OutputStreamWriter(socket.getOutputStream())),
+                    true);
+            out.println("clicPiocheDonjon");
+        } catch (UnknownHostException e) {
+            e.printStackTrace();
+        } catch (IOException e) {
+            e.printStackTrace();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+
     }
 }
