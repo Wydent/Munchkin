be788256b781ee16928dd4068764453ff1096d88
JonathanDevinAliasMaurice <jdevin62@gmail.com>
2016-02-18 23:28:54 +0100

    Fonction init_carte
    test init_cartes + fichier texte contenant les cartes

--- MunchkinMaxime/Serveur java/src/Test.java
+++ MunchkinMaxime/Serveur java/src/Test.java
@@ -1,3 +1,8 @@
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.FileInputStream;
+import java.io.InputStream;
+import java.io.InputStreamReader;
 import java.lang.reflect.InvocationTargetException;
 import java.lang.reflect.Method;
 import java.util.ArrayList;
@@ -6,8 +11,59 @@ import java.util.HashMap;
 
 
 public class Test {
+	public static void init_cartes(String path,Partie p){
+		String[] chaine;
+		
+		ArrayList<Carte> tresors=new ArrayList<Carte>();
+		ArrayList<Carte> donjons=new ArrayList<Carte>();
+		try{
+			InputStream ips=new FileInputStream(path); 
+			InputStreamReader ipsr=new InputStreamReader(ips);
+			BufferedReader br=new BufferedReader(ipsr);
+			String ligne;
+			while ((ligne=br.readLine())!=null){
+				chaine=ligne.split(";");
+				Carte c = null ;
+				if(chaine[1]=="carte"){
+					c=new Carte(chaine[2],chaine[3],chaine[4],null,chaine[0]);
+				}
+				else if(chaine[1]=="monstre"){
+					c=new Monstre(chaine[2],chaine[3],chaine[4],null,chaine[0],Integer.parseInt(chaine[8]),Integer.parseInt(chaine[9]),null,Integer.parseInt(chaine[6]));
+				}
+				else if(chaine[1]=="equipement"){
+					c=new Equipement(chaine[2],chaine[3],chaine[4],null,chaine[0],chaine[6],Boolean.getBoolean(chaine[7]),null);
+				}
+				else if(chaine[1]=="malediction"){
+					c=new Malediction(chaine[2],chaine[3],chaine[4],null,chaine[0],null,Integer.parseInt(chaine[7]));
+				}
+				else if(chaine[1]=="race"){
+					
+				}
+				else if(chaine[1]=="classe"){
+					
+				}
+				
+				if(c.getType()=="donjon"){
+					donjons.add(c);
+				}
+				else if(c.getType()=="tresor"){
+					tresors.add(c);
+				}
+				p.setPaquet_donjons(donjons);
+				p.setPaquet_tresors(tresors);
+			}
+			br.close(); 
+			System.out.println("taille paquet donjon " + p.getPaquet_donjons().size());
+			System.out.println("Carte : "+p.getPaquet_donjons().get(0));
+		}		
+		catch (Exception e){
+			System.out.println(e.toString());
+		}
+		
+		
+	}
 	public static void main(String[] args) throws NoSuchMethodException, SecurityException, InstantiationException, IllegalAccessException{
-		Object t = Effet.class.newInstance();
+/*	Object t = Effet.class.newInstance();
 	Carte c1= new Monstre("justin bibiere","justin qui boit de la biere","special",new ArrayList<Method>(),"monstre",4,2,null,12);
 	Carte c2 = new Carte("laver la voiture du mj","+1 niveau", "tontour", new ArrayList<Method>(), "bonus");
 	Joueur j1 = new Joueur("jojo");
@@ -36,7 +92,8 @@ public class Test {
 		}
 	}
 	
-	System.out.println("Test après effet : "+j1.getNiveau());
-	
+	System.out.println("Test après effet : "+j1.getNiveau());*/
+	Partie p = new Partie();
+	init_cartes("cartes.txt",p);
 	}
 }
