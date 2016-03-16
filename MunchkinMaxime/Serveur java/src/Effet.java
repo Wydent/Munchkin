83f1eb70c59cce115f927eb248a47c04308e6791
JonathanDevinAliasMaurice <jdevin62@gmail.com>
2016-03-16 13:47:27 +0100

    ajout effet

--- MunchkinMaxime/Serveur java/src/Effet.java
+++ MunchkinMaxime/Serveur java/src/Effet.java
@@ -1,3 +1,5 @@
+import java.util.ArrayList;
+
 
 
 /**
@@ -5,9 +7,34 @@
  */
 public class Effet {
 	public void modifierNiveau(Integer nbniveau,Joueur joueur){
-		joueur.setNiveau(joueur.getNiveau()+nbniveau);
+		int niveau=joueur.getNiveau()+nbniveau;
+		if(niveau<1){
+		joueur.setNiveau(1);
+		}else{
+		joueur.setNiveau(niveau);
+		}
+			
 	}
 	public void modifierAttaque(Integer nbniveau,Joueur joueur){
 		joueur.setAttaque(joueur.getAttaque()+nbniveau);
+	}
+	public void mort(Joueur joueur){
+		joueur.setNiveau(1);
+		joueur.setAttaque(1);
+		joueur.setClasses(new ArrayList<Classe>());
+		joueur.setRaces(new ArrayList<Race>());
+		joueur.setEquipements(new ArrayList<Equipement>());
+		joueur.setDeguerpir(0);
+		joueur.setEstSangMelee(false);
+		joueur.setEstSuperMunckhin(false);
+		joueur.setMain(new ArrayList<Carte>());
+		
+	}
+	public void perte_equipement(String emplacement,Joueur joueur){
+		for(Equipement e:joueur.getEquipements()){
+			if(e.getPartie_corps().equals(emplacement)){
+				joueur.getEquipements().remove(e);
+			}
+		}
 	}
 }
